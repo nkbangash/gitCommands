d1273c7789b7c6853e0b91f6f8c74e2330f85f51
Nasir Bangash <nkbangash@gmail.com>
2023-02-08 13:39:58 -0500

    added 96

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function add(num1, num2) {
 }
 
 console.log(add(2, 3));
-console.log(add(3, 6));
+console.log(add(9, 6));
 
 if (true) {
-  console.log('Technosoft is the best school in town. so glad I decided to join');
+  console.log(
+    'Technosoft is the best school in town. so glad I decided to join'
+  );
 }
